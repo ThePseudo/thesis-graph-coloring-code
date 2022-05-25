31bef37da3e84b70666165f36274ed614d862f17
Alessandro Borione <leax_xiv@me.com>
2022-05-25 19:11:17 +0200

    Use thrust::device_vector
    Compute  non-colored nodes directly on the GPU with thrust library

--- GraphColoring/cudakernels/cudaKernels.cu
+++ GraphColoring/cudakernels/cudaKernels.cu
@@ -1,6 +1,8 @@
 ﻿#include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include <thrust/count.h>
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
 
 #include <cuda_runtime_api.h>
 #include <cuda.h>
@@ -24,31 +26,29 @@ inline void cudaSafeCheck(cudaError_t call, const char *file, int line, bool abo
   }
 }
 
-int launch_kernel(int n, int* dAo, int* dAc, int* dRandoms, int* dColors, int* colors);
-int launch_kernel_coop(int n, const int* dAo, const int* dAc, const int* dRandoms, int* dColors, int* colors);
-
+int launch_kernel(int n, int* dAo, int* dAc, int* dRandoms, thrust::device_vector<int>& dvColors, int* colors);
 __global__ void color_jpl_kernel(const int n, const int c, const int* Ao, const int* Ac, const int* randoms, int* colors);
-//__global__ void create_independent_set_kernel(int n, const int* Ao, const int* Ac, const int* randoms, const int* colors, unsigned int* set);
-//__global__ void expand_to_maximal_independent_set_kernel(int n, const int* Ao, const int* Ac, const int* colors, unsigned int* set);
-//__global__ void color_jpl_kernel(int n, int c, int* colors, const unsigned int* set);
+
+int launch_kernel_coop(int n, const int* dAo, const int* dAc, const int* dRandoms, int* dColors, int* colors);
 __global__ void color_jpl_coop_kernel(int n, const int* Ao, const int* Ac, const int* randoms, int* colors);
+
+__device__ bool color_jpl_ingore_neighbor(const int c, const int i, const int j, const int jc);
+__device__ bool color_jpl_assign_color(const int c, int* color_i, const bool localmax, const bool localmin = false);
 
 int color_jpl(int const n, const int* Ao, const int* Ac, int* colors, const int* randoms) {
 	int* dAo;
 	int* dAc;
 	int* dRandoms;
-	int* dColors;
 	Benchmark& bm = *Benchmark::getInstance();
+	thrust::device_vector<int> dvColors(n, -1);
 
 	CUDA_SAFE_CALL(cudaMalloc(&dAo, (n + 1) * sizeof(*dAo)));
 	CUDA_SAFE_CALL(cudaMalloc(&dAc, Ao[n] * sizeof(*dAc)));
 	CUDA_SAFE_CALL(cudaMalloc(&dRandoms, n * sizeof(*dRandoms)));
-	CUDA_SAFE_CALL(cudaMalloc(&dColors, n * sizeof(*dColors)));
 
 	CUDA_SAFE_CALL(cudaMemcpy(dAo, Ao, (n + 1) * sizeof(*Ao), cudaMemcpyHostToDevice));
 	CUDA_SAFE_CALL(cudaMemcpy(dAc, Ac, Ao[n] * sizeof(*Ac), cudaMemcpyHostToDevice));
 	CUDA_SAFE_CALL(cudaMemcpy(dRandoms, randoms, n * sizeof(*randoms), cudaMemcpyHostToDevice));
-	CUDA_SAFE_CALL(cudaMemcpy(dColors, colors, n * sizeof(*colors), cudaMemcpyHostToDevice));
 
 	bm.sampleTimeToFlag(2);
 
@@ -59,49 +59,91 @@ int color_jpl(int const n, const int* Ao, const int* Ac, int* colors, const int*
 	int c = -1;
 	if (supportsCoopLaunch) {
 		std::cout << "Launching in cooperative mode🤝" << std::endl;
+		// Get raw pointer for dColors
+		int* dColors = thrust::raw_pointer_cast(dvColors.data());
 		c = launch_kernel_coop(n, dAo, dAc, dRandoms, dColors, colors);
 	} else {
-		c = launch_kernel(n, dAo, dAc, dRandoms, dColors, colors);
+		c = launch_kernel(n, dAo, dAc, dRandoms, dvColors, colors);
 	}
-	//c = launch_kernel_coop(n, dAo, dAc, dRandoms, dColors, colors);
 
 	CUDA_SAFE_CALL(cudaFree(dAo));
 	CUDA_SAFE_CALL(cudaFree(dAc));
 	CUDA_SAFE_CALL(cudaFree(dRandoms));
-	CUDA_SAFE_CALL(cudaFree(dColors));
 
 	return c;
 }
 
-int launch_kernel(int n, int* dAo, int* dAc, int* dRandoms, int* dColors, int* colors) {
-	int device = 0;
-	cudaDeviceProp deviceProp;
-	cudaGetDeviceProperties(&deviceProp, device);
+int launch_kernel(int n, int* dAo, int* dAc, int* dRandoms, thrust::device_vector<int>& dvColors, int* colors) {
 	Benchmark& bm = *Benchmark::getInstance();
-	int c = -1;
-	int left;
-	
-	int const nt = 256;
-	int nb = std::min((n + nt - 1) / nt, deviceProp.maxGridSize[0]);
+	int c = -1;	// Number of colors used
+	int left = (int)thrust::count(dvColors.begin(), dvColors.end(), -1);	// Number of non-colored vertices
 
-	for (c = 0, left = n; left > 0 && c < n; ++c) {
-		bm.sampleTimeToFlag(4);
+	int nb;	// Number of blocks to be launched
+	int nt;	// Number of threads per block to be launched
+	// Get optimal number of blocks and threads to launch to fill SMs
+	cudaOccupancyMaxPotentialBlockSize(&nb, &nt, color_jpl_kernel, 0, 0);
+	// Limit blocks ti be launched by the number of vertices
+	nb = std::min((n + nt - 1) / nt, nb);
 
+	int* dColors = thrust::raw_pointer_cast(dvColors.data());
+	for (c = 0; left > 0 && c < n; ++c) {
+		// Launch coloring iteration kernel
 		color_jpl_kernel<<<nb, nt>>>(n, c, dAo, dAc, dRandoms, dColors);
-		//create_independent_set_kernel<<<nb, nt>>>(n, dAo, dAc, dRandoms, dColors, dSet);
-		//expand_to_maximal_independent_set_kernel<<<nb, nt>>>(n, dAo, dAc, dColors, dSet);
-		//color_jpl_kernel<<<nb, nt>>>(n, c, dColors, dSet);
-
 		cudaDeviceSynchronize();
 		bm.sampleTimeToFlag(1);
 
-		CUDA_SAFE_CALL(cudaMemcpy(colors, dColors, n * sizeof(*colors), cudaMemcpyDeviceToHost));
-		bm.sampleTimeToFlag(3);
-
-		left = (int)thrust::count(colors, colors + n, -1);
+		// Count non-colored vertices (on GPU)
+		left = (int)thrust::count(dvColors.begin(), dvColors.end(), -1);
+		bm.sampleTimeToFlag(4);
 	}
 
+	// Copy colors array from GPU
+	thrust::copy(dvColors.begin(), dvColors.end(), colors);
+	bm.sampleTimeToFlag(3);
+
 	return c;
+}
+
+__global__ void color_jpl_kernel(const int n, const int c, const int* Ao, const int* Ac, const int* randoms, int* colors) {
+	for (int i = threadIdx.x + blockIdx.x * blockDim.x;
+		i < n;
+		i += blockDim.x * gridDim.x)
+	{
+
+		int color = c;
+		// true if you have max random
+		bool localmax = true;
+#ifdef COLOR_MIN_MAX_INDEPENDENT_SET
+		// true if you have min random
+		bool localmin = true;
+		color *= 2;
+#endif
+
+		// ignore nodes colored earlier
+		if (colors[i] != -1) continue;
+
+		// look at neighbors to check their random number
+		for (int k = Ao[i]; k < Ao[i + 1]; k++) {
+			// ignore nodes colored earlier (and yourself)
+			int j = Ac[k];
+
+			if (color_jpl_ingore_neighbor(color, i, j, colors[j])) continue;
+
+			int ir = randoms[i];
+			int jr = randoms[j];
+
+			localmax &= ir > jr;
+#ifdef COLOR_MIN_MAX_INDEPENDENT_SET
+			localmin &= ir < jr;
+#endif
+		}
+		// assign color if you have the maximum (or minimum) random number
+#ifdef COLOR_MIN_MAX_INDEPENDENT_SET
+		color_jpl_assign_color(color, &colors[i], localmax, localmin);
+#elif defined(COLOR_MAX_INDEPENDENT_SET)
+		color_jpl_assign_color(color, &colors[i], localmax);
+#endif
+	}
 }
 
 int launch_kernel_coop(int n, const int* dAo, const int* dAc, const int* dRandoms, int* dColors, int* colors) {
@@ -144,24 +186,27 @@ __global__ void color_jpl_coop_kernel(int n, const int* Ao, const int* Ac, const
 			i < n;
 			i += blockDim.x * gridDim.x)
 		{
+
+			int color = c;
 			// true if you have max random
 			bool localmax = true;
 #ifdef COLOR_MIN_MAX_INDEPENDENT_SET
 			// true if you have min random
 			bool localmin = true;
+			color *= 2;
 #endif
-			
-		// ignore nodes colored earlier
-			if (colors[i] != -1) continue;
 
-			int ir = randoms[i];
+			// ignore nodes colored earlier
+			if (colors[i] != -1) continue;
 
 			// look at neighbors to check their random number
 			for (int k = Ao[i]; k < Ao[i + 1]; k++) {
 				// ignore nodes colored earlier (and yourself)
 				int j = Ac[k];
-				int jc = colors[j];
-				if ((jc != -1 && jc != c) || (i == j)) continue;
+
+				if (color_jpl_ingore_neighbor(color, i, j, colors[j])) continue;
+
+				int ir = randoms[i];
 				int jr = randoms[j];
 
 				localmax &= ir > jr;
@@ -171,105 +216,32 @@ __global__ void color_jpl_coop_kernel(int n, const int* Ao, const int* Ac, const
 			}
 			// assign color if you have the maximum (or minimum) random number
 #ifdef COLOR_MIN_MAX_INDEPENDENT_SET
-			if (localmin) colors[i] = 2 * c + 1;
-			if (localmax) colors[i] = 2 * c;
-			if (!localmax && !localmin) ++left;
-#endif
-#ifdef COLOR_MAX_INDEPENDENT_SET
-			if (localmax) colors[i] = c;
-			else ++left;
+			if (color_jpl_assign_color(color, &colors[i], localmax, localmin)) ++left;
+#elif defined(COLOR_MAX_INDEPENDENT_SET)
+			if (color_jpl_assign_color(color, &colors[i], localmax)) ++left;
 #endif
 		}
 		grid.sync();
 	}
 }
 
-__global__ void color_jpl_kernel(const int n, const int c, const int* Ao, const int* Ac, const int* randoms, int* colors) {
-	for (int i = threadIdx.x + blockIdx.x * blockDim.x;
-		i < n;
-		i += blockDim.x * gridDim.x)
-	{
-		// true if you have max random
-		bool localmax = true;
+__device__ bool color_jpl_ingore_neighbor(const int c, const int i, const int j, const int jc) {
 #ifdef COLOR_MIN_MAX_INDEPENDENT_SET
-		// true if you have min random
-		bool localmin = true;
+	return ((jc != -1) && (jc != c) && (jc != c + 1)) || (i == j);
+#elif defined(COLOR_MAX_INDEPENDENT_SET)
+	return ((jc != -1) && (jc != c)) || (i == j);
 #endif
-
-		// ignore nodes colored earlier
-		if (colors[i] != -1) continue;
-
-		int ir = randoms[i];
-
-		// look at neighbors to check their random number
-		for (int k = Ao[i]; k < Ao[i + 1]; k++) {
-			// ignore nodes colored earlier (and yourself)
-			int j = Ac[k];
-			int jc = colors[j];
-			if ((jc != -1 && jc != c) || (i == j)) continue;
-			int jr = randoms[j];
-
-			//localmax &= ir > jr;
-			if (ir <= jr) localmax = false;
-#ifdef COLOR_MIN_MAX_INDEPENDENT_SET
-			localmin &= ir < jr;
-#endif
-		}
-		// assign color if you have the maximum (or minimum) random number
-#ifdef COLOR_MIN_MAX_INDEPENDENT_SET
-		if (localmin) colors[i] = 2 * c + 1;
-		if (localmax) colors[i] = 2 * c;
-		//if (!localmax && !localmin) ++left;
-#endif
-#ifdef COLOR_MAX_INDEPENDENT_SET
-		if (localmax) colors[i] = c;
-		//else ++left;
-#endif
-	}
 }
 
-//__global__ void expand_to_maximal_independent_set_kernel(int n, const int* Ao, const int* Ac, const int* colors, unsigned int* set) {
-//	for (int i = threadIdx.x + blockIdx.x * blockDim.x;
-//		i < n;
-//		i += blockDim.x * gridDim.x)
-//	{
-//		// Ignore nodes colored earlier or already in set
-//		if (colors[i] != -1 || set[i] != 0x0) continue;
-//
-//		set[i] = 0x2;
-//
-//		for (int k = Ao[i]; k < Ao[i + 1]; k++) {
-//			// ignore nodes colored earlier (and yourself)
-//			int j = Ac[k];
-//			int jc = colors[j];
-//			if ((jc != -1) || (i == j)) continue;
-//			// cannot be part of MIS if neighbor is in initial set
-//			//  or if neighboring vertex with higher degree is trying to enter the MIS
-//			if (set[j] == 0x1 ||
-//				(set[j] == 0x2 && Ao[i + 1] - Ao[i] <= Ao[j + 1] - Ao[j])
-//				)
-//				set[i] = 0x0;
-//		}
-//
-//		if (set[i] != 0x0) set[i] = 0x1;
-//	}
-//}
-//
-//__global__ void color_jpl_kernel(int n, int c, int* colors, const unsigned int* set) {
-//	for (int i = threadIdx.x + blockIdx.x * blockDim.x;
-//		i < n;
-//		i += blockDim.x * gridDim.x)
-//	{
-//#ifdef COLOR_MIN_MAX_INDEPENDENT_SET
-//		if (colors[i] != -1 || set[i] == 0 || set[i] == 3) continue;
-//		colors[i] = 2 * c + set[i] - 1;
-//#endif
-//#ifdef COLOR_MAX_INDEPENDENT_SET
-//		if (colors[i] != -1) continue;
-//		if(set[i] != 0) colors[i] = c;
-//#endif
-//	}
-//}
+__device__ bool color_jpl_assign_color(const int c, int* color_i, const bool localmax, const bool localmin) {
+#ifdef COLOR_MIN_MAX_INDEPENDENT_SET
+	if (localmin) *color_i = c + 1;
+	else
+#endif
+	if (localmax) *color_i = c;
+
+	return localmax || localmin;
+}
 
 int color_cusparse(int const n, const int* Ao, const int* Ac, int* colors) {
 	float* dAv;
