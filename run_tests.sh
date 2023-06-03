b5860a99470035780f1d66b5d956f376ac08a5bf
Andrea Calabrese <andrea1995.c@live.com>
2023-06-03 22:38:22 +0200

    Heating up before tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
-mkdir results
+#mkdir results
+#./build/GraphColoring/jp_cuda_double -r 20 /home/cardone/Desktop/graphs/gra/apache2.gra > /dev/null
+#for graph in /home/cardone/Desktop/graphs/gra/* ; do
+#    graph_name=${graph##*/}
+#    echo "Stream on double: $graph_name..."
+#    mkdir results/$graph_name
+#    ./build/GraphColoring/jp_cuda_double -r 20 $graph > results/$graph_name/double_stream.txt
+#done
+#
+#./build_no_stream/GraphColoring/jp_cuda_double -r 20 /home/cardone/Desktop/graphs/gra/apache2.gra > /dev/null
+#for graph in /home/cardone/Desktop/graphs/gra/* ; do
+#    graph_name=${graph##*/}
+#    echo "Vanilla on double: $graph_name..."
+#    ./build_no_stream/GraphColoring/jp_cuda_double -r 20 $graph > results/$graph_name/double_no_stream.txt
+#done
 
+./build/GraphColoring/jp_cuda_single -r 20 /home/cardone/Desktop/graphs/gra/apache2.gra > /dev/null
 for graph in /home/cardone/Desktop/graphs/gra/* ; do
-graph_name=${graph##*/}
-echo "Working on $graph_name..."
-mkdir results/$graph_name
-#./build/GraphColoring/jp_cuda_double -r 20 $graph > results/$graph_name/double_stream.txt
-#./build_no_stream/GraphColoring/jp_cuda_double -r 20 $graph > results/$graph_name/double_no_stream.txt
-./build/GraphColoring/jp_cuda_single -r 20 $graph > results/$graph_name/single_stream.txt
-./build_no_stream/GraphColoring/jp_cuda_single -r 20 $graph > results/$graph_name/single_no_stream.txt
+    graph_name=${graph##*/}
+    echo "Stream on single: $graph_name..."
+    ./build/GraphColoring/jp_cuda_single -r 20 $graph > results/$graph_name/single_stream.txt
+done
+
+./build_no_stream/GraphColoring/jp_cuda_single -r 20 /home/cardone/Desktop/graphs/gra/apache2.gra > /dev/null
+for graph in /home/cardone/Desktop/graphs/gra/* ; do
+    graph_name=${graph##*/}
+    echo "Vanilla on single: $graph_name..."
+    ./build_no_stream/GraphColoring/jp_cuda_single -r 20 $graph > results/$graph_name/single_no_stream.txt
 done
